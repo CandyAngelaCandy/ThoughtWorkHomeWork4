852b9d2e27a00daa033aaba55b8e58b088939153
Candy <229355694@qq.com>
2017-05-23 17:29:48 +0800

    first commit

--- main/main.js
+++ main/main.js
@@ -1,3 +1,7 @@
 module.exports = function main() {
   // Write your cade here
+  var sum=arguments[0].split("").reduce(function (prev,cur) {
+    return parseInt(prev)+parseInt(cur);
+  });
+  return sum;
 };
